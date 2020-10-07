bd450c061e2c822ab9c8e2307aa6a8458f977403
himanoa <matsunoappy@gmail.com>
2020-10-08 03:22:09 +0900

    implemented pr-id

---
+++ commands/pr-id.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+pr-id() {
+  gh api graphql -f query="
+    query {
+      repository(owner: \"$1\", name:\"$2\") {
+        pullRequests(headRefName: \"$3\", first: 100) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    }
+  " | jq .data.repository.pullRequests.edges[0].node.id
+}
+
+
+pr-id "$@"
