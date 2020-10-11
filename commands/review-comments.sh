c57cb7eb32a2dd20b48d4075a9c8948f4dfe18e0
Chitoku <odango@chitoku.jp>
2020-10-12 00:21:26 +0900

    Fix #7: Implement line-to-position

--- commands/review-comments.sh
+++ commands/review-comments.sh
@@ -4,11 +4,21 @@
 review-comments() {
   local REVIEW_COMMENT_STATE="./.REVIEW_COMMENT_STATE"
   local current=$(cat -- "$REVIEW_COMMENT_STATE" 2> /dev/null)
+  local path=$1
+  local line=$2
+  local body=$3
+  local position
+
+  # TODO: Change upstream branch (currently `master`) to correct one!
+  # TODO: Change `add` to correct one!
+  if ! position=$(git diff --diff-algorithm=default master -- "$path" | "$(dirname -- "$0")"/line-to-position "$line" add); then
+    return 1
+  fi
 
   jq \
-    --arg path "$1" \
-    --arg position "$2" \
-    --arg body "$3" \
+    --arg path "$path" \
+    --arg position "$position" \
+    --arg body "$body" \
     'flatten + [{path: $path, position: $position | tonumber, body: $body}]' \
     <<< "${current:-[]}" > "$REVIEW_COMMENT_STATE"
 }
