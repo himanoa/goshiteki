860c15cbee87938f22213a6f248ae788e88e6aaa
himanoa <matsunoappy@gmail.com>
2020-10-14 00:51:14 +0900

    review-comments.sh support multi-line review comment

--- commands/review-comments.sh
+++ commands/review-comments.sh
@@ -5,27 +5,45 @@ review-comments() {
   local REVIEW_COMMENT_STATE="./.REVIEW_COMMENT_STATE"
   local current=$(cat -- "$REVIEW_COMMENT_STATE" 2> /dev/null)
   local path=$1
-  local line=$2
-  local body=$3
-  local base_branch=$4
+  local start_line=$2
+  local end_line=$3
+  local body=$4
+  local base_branch=$5
 
-  if ! git diff --diff-algorithm=default "$base_branch" -- "$path" | "$(dirname -- "$0")"/line-in-range.sh "$line" add; then
+  if ! git diff --diff-algorithm=default "$base_branch" -- "$path" | "$(dirname -- "$0")"/line-in-range.sh "$start_line" add; then
     return 1
   fi
 
-  jq \
-    --arg path "$path" \
-    --arg line "$line" \
-    --arg body "$body" '
-    flatten + [
-      {
-        path: $path,
-        line: $line | tonumber,
-        body: $body,
-        startSide: "RIGHT",
-        side: "RIGHT"
-      }
-    ]' <<< "${current:-[]}" > "$REVIEW_COMMENT_STATE"
+  if [ "$start_line" = "$end_line" ]; then
+    jq \
+      --arg path "$path" \
+      --arg line "$line" \
+      --arg body "$body" '
+      flatten + [
+        {
+          path: $path,
+          line: $start_line | tonumber,
+          body: $body,
+          startSide: "RIGHT",
+          side: "RIGHT"
+        }
+      ]' <<< "${current:-[]}" > "$REVIEW_COMMENT_STATE"
+  else
+    jq \
+      --arg path "$path" \
+      --arg line "$line" \
+      --arg body "$body" '
+      flatten + [
+        {
+          path: $path,
+          start_line: $start_line | tonumber,
+          line: $end_line | tonumber,
+          body: $body,
+          startSide: "RIGHT",
+          side: "RIGHT"
+        }
+      ]' <<< "${current:-[]}" > "$REVIEW_COMMENT_STATE"
+  fi
 }
 
 review-comments "$@"
